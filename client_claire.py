9be400b8e1d117d65930d4d25ad227a1fb6309d6
Hamonet Claire <chamonet@SA203-07A.ensi-bourges.fr>
2017-04-25 10:10:12 +0200

    modifié :         client_claire.py 	modifié :         server_claire.py

---
+++ client_claire.py
@@ -0,0 +1,366 @@
+#!/usr/bin/env python
+#-*-coding: utf8-*
+import subprocess as sub
+import commands as cmd
+import socket
+import time
+import os
+import json
+import sys
+import getpass
+import graphical_claire
+import accueil
+import interface_reseau
+
+current_dir=os.getcwd()
+os.chdir(current_dir) #le path change automatiquement                    
+
+correct=True
+blank=True
+length=True
+nb=True
+lt=True
+already_register=True
+pwd=True
+gr=True
+
+def securite(password):
+    if len(password)<8:
+        length=False
+        nb=True
+        lt=True
+        save=graphical_claire.fenetresave(blank, length, nb, lt, already_register, pwd, gr)
+        register_client(save[1], save[2], save[3], save [4])
+        length=True
+        print("Your password is not long enough. (8 caracters min.)")
+        return False 
+    chiffre=False
+    lettre=False
+    for i in password:
+        if i.isdigit():
+            chiffre=True
+    for i in password:
+        if i.isalpha():
+            lettre=True
+    if chiffre==False:
+        nb=False
+        length=True
+        lt=True
+        save=graphical_claire.fenetresave(blank, length, nb, lt, already_register, pwd, gr)
+        register_client(save[1], save[2], save[3], save [4])
+        nb=True
+        print("Your password has to contain a number.")
+        return False
+    if lettre==False:
+        lt=False
+        length=True
+        nb=True
+        save=graphical_claire.fenetresave(blank, length, nb, lt, already_register, pwd, gr)
+        register_client(save[1], save[2], save[3], save [4])
+        print("Your password has to contain a letter.")
+        lt=True
+        return False
+    return True
+
+def login(username, password):
+    print("Please enter your login")
+    while True:
+        #username = raw_input("Username: ")
+        if not len(username) > 0:
+            correct=False
+            auth=graphical_claire.fenetreauth(correct)
+            login(auth[1], auth[2])
+            correct=True
+            print("Username can't be blank")
+        else:
+            break
+    while True:
+        #password = getpass.getpass("Password : ")
+        if not len(password) > 0:
+            correct=False
+            auth=graphical_claire.fenetreauth(correct)
+            login(auth[1], auth[2])
+            correct=True
+            print("Password can't be blank")
+        else:
+            break
+    data = "login " + username + " " + password
+    s.send(data)
+    time.sleep(1)
+    data=s.recv(BUFFER_SIZE)
+    if data=="true":
+        return session(username)
+    elif data == "false":
+        correct=False
+        auth=graphical_claire.fenetreauth(correct)
+        login(auth[1], auth[2])
+        correct=True
+        print("Invalid username or password! you redirect to home")
+
+def register_client(username, password, password2, group):
+    while True:
+        #username = raw_input("New username: ")
+        if not len(username) > 0:
+            blank=False
+            save=graphical_claire.fenetresave(blank, length, nb, lt, already_register, pwd, gr)
+            register_client(save[1], save[2], save[3], save [4])
+            blank=True
+            print("Username can't be blank")
+            continue
+        else:
+            break
+    while True:
+        #password = getpass.getpass("Password : ")
+        #password2 = getpass.getpass("Re-enter password : ")
+        if password!=password2 :
+            pwd=False
+            print("Passwords don't match.\nPlease try again.\n")
+            save=graphical_claire.fenetresave(blank, length, nb, lt, already_register, pwd, gr)
+            register_client(save[1], save[2], save[3], save [4])
+            pwd=True
+            continue
+        if not securite(password):
+            print("Please try again.\n")
+            continue
+        else:
+            break
+    while True:
+        #group = raw_input("choice your group (doctor, nurse, secretary) : ")
+        if not len(group) > 0:
+            print("group can't be blank")
+            gr=False
+            save=graphical_claire.fenetresave(blank, length, nb, lt, already_register, pwd, gr)
+            register_client(save[1], save[2], save[3], save [4])
+            gr=True
+            continue
+        else:
+            break
+    print("Creating account...")
+    data="register " + username + " " + password + " " + group
+    s.send(data)
+    time.sleep(1)
+    data=s.recv(BUFFER_SIZE)
+    if data=="uncreated":
+        already_register=False
+        print("Username already use! Please recover your password or contact your administration")
+        save=graphical_claire.fenetresave(blank, length, nb, lt, already_register, pwd, gr)
+        register_client(save[1], save[2], save[3], save [4])
+        already_register=True
+        login()
+    elif data == "created":
+        print("User " + username + " created")
+        login(username, password)
+
+#arefaire
+
+
+def sendmail_client(username):
+    while True:
+        recipient = raw_input("Recipient > ")
+        if not len(recipient) > 0:
+            print("Recipient can't be blank")
+            continue
+        s.send(recipient)
+        time.sleep(1)
+        valid=s.recv(BUFFER_SIZE)
+        if valid=="false":
+            print("There is no account with that username")
+            continue
+        else:
+            break
+    while True:
+        subject = raw_input("Subject > ")
+        if not len(subject) > 0:
+            print("Subject can't be blank")
+            continue
+        else:
+            break
+    while True:
+        context = raw_input("Context > ")
+        if not len(context) > 0:
+            print("Context can't be blank")
+        else:
+            break
+    print("Sending mail...")
+    s.send(subject)
+    time.sleep(1)
+    s.send(context)
+    time.sleep(1)
+    print("Mail has been sent to " + recipient)
+
+
+def shell(data):                            #Attention commande cd
+    a=sub.check_output(data, shell=True)
+    if (a==''):
+        a='commande reussie'
+    return a
+
+def sending(file):
+    f=open(file,"rb")
+    l = f.read(1024)
+    while (l):
+        print 'Sending...'
+        s.send(l)
+        l = f.read(1024)
+    f.close()
+    print "Done Sending"
+
+def receiving(file):
+    try:
+        f=open(file,"w")
+    except IOError :
+        sub.call("touch " + file, shell=True )
+    finally:
+        l = s.recv(1024)
+        while(l):
+            print "receiving"
+            f.write(l)
+            l=s.recv(BUFFER_SIZE)
+        f.close()
+
+
+def session(username):
+    current_dir=os.getcwd()
+    os.chdir(current_dir) #le path change automatiquement                        
+    data="session " + username
+    s.send(data)
+    time.sleep(1)
+    print("Welcome to your account " + username)
+    message_session=s.recv(BUFFER_SIZE)
+    print(message_session)
+    while True:
+        print("Options: view mail | send mail | commande shell | gestion de fichier | logout")
+        inter=interface_reseau.interface(current_dir, username)
+        print("inter", inter[0], inter[1], inter[2], inter[3])
+        if message_session.split(" ")[1] == "admin":
+            print("options user mail | delete mail | delete account")
+        #option = raw_input(username + " > ")
+        s.send(option)
+        time.sleep(1)
+        if inter[0]==False:
+            print("Logging out...")
+            break
+        elif option == "view mail":
+            print("Current mail:")
+            nombre_mail=s.recv(BUFFER_SIZE)
+            for i in range(int(nombre_mail)):
+                mail=s.recv(BUFFER_SIZE)
+                print(mail)
+        elif option == "send mail":
+            sendmail_client(username)
+        elif option == "commande shell":
+            commande=raw_input("Please enter your commande ! ")
+            s.send(commande)
+            time.sleep(1)
+            resultats=s.recv(BUFFER_SIZE)
+            print(resultats)
+        elif inter[1] == False:
+            while True:
+                print("Options: creer un rapport | lire un rapport | retour")
+                option_fichier=raw_input(username + " > ")
+                s.send(option_fichier)
+                time.sleep(1)
+                if option_fichier=="creer un rapport":#le fichier est enregister chez le client......a modifier
+                    title=raw_input("Enter your title of file : ")
+                    title=title+".odt"
+                    s.send(title)
+                    fichier=open(title,'w')
+                    fichier.close()
+                    data="libreoffice " + title
+                    data=shell(data)
+                    terminer=raw_input("Avez vous terminez ?(oui ou non)")
+                    while terminer!="oui":
+                        time.sleep(1)
+                    sending(title)
+
+                elif inter[2]== 2:#le fichier est chez le client ...a modifier
+                    #commande=raw_input("Please enter your filename : ")
+                    commande=inter[3]
+                    data="libreoffice " + commande + "*"
+                    data=sub.call(data, shell=True)
+                elif option_fichier=="retour":
+                    break
+
+        elif message_session.split(" ")[1] == "admin":
+            if option == "user mail":
+                print("Whos mail would you like to see?")
+                userinfo = raw_input("> ")
+                s.send(userinfo)
+                time.sleep(1)
+                info=s.recv(BUFFER_SIZE)
+                if info=="true":
+                    nombre_mail=s.recv(BUFFER_SIZE)
+                    for i in range(int(nombre_mail)):
+                        mail=s.recv(BUFFER_SIZE)
+                        print(mail)
+                else:
+                    print("There is no account with that username")
+                time.sleep(1)
+           
+            elif option == "delete mail":
+                print("Whos mail would you like to delete?")
+                userinfo = raw_input("> ")
+                s.send(userinfo)
+                time.sleep(1)
+                info=s.recv(BUFFER_SIZE)
+                if info=="true":
+                    print("Deleting " + userinfo + "'s mail...")
+                    time.sleep(1)
+                    print(userinfo + "'s mail has been deleted")
+                else:
+                    print("There is no account with that username")
+                time.sleep(1)
+
+            elif option == "delete account":
+                print("Whos account would you like to delete?")
+                userinfo = raw_input("> ")
+                s.send(userinfo)
+                time.sleep(1)
+                info=s.recv(BUFFER_SIZE)
+                if info=="true":
+                    print("Are you sure you want to delete " + userinfo + "'s account?")
+                    print("Options: yes | no")
+                    while True:
+                        confirm=raw_input(" > ")
+                        s.send(confirm)
+                        time.sleep(1)
+                        response=s.recv(BUFFER_SIZE)
+                        print(response)
+                        break
+                else:
+                    print("There is no account with that username")
+                time.sleep(1)
+
+        else:
+            print(option + " is not an option")
+
+            
+
+
+TCP_IP = '0.0.0.0'
+TCP_PORT = 6264
+BUFFER_SIZE = 1024
+
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+while 1:
+    option=accueil.accueil()
+    print("Welcome to the system. Please register or login.")
+    print("Options: register | login | exit")
+    print option
+    while True:
+        if option == "login":
+            auth=graphical_claire.fenetreauth(True)
+            login(auth[1], auth[2])
+        elif option == "register":
+            save=graphical_claire.fenetresave(True, True, True, True, True, True, True)
+            register_client(save[1], save[2], save[3], save [4])
+        elif option == "exit":
+            break
+        else:
+            print(option + " is not an option")
+
+
+s.close()
+print("received data")
---
+++ server_claire.py
@@ -0,0 +1,360 @@
+#!/usr/bin/env python
+#-*-coding: utf8-*
+
+import socket
+from threading import Thread
+from SocketServer import ThreadingMixIn
+import subprocess as sub
+import commands as cmd
+import time
+import os
+import json
+import sys
+
+
+DEFAULT_FILEPATH = 'comptes.json'
+#all accounts
+users = {
+    "root": {
+        "password": "gucci-mane",
+        "group": "admin",
+        "mail": []
+    }
+}
+
+#save account in file
+#os.chdir("/promo2019/chamonet/Insa/projetreseau/projet_reseau") #a changer en fonction de l'installation                       
+#os.chdir("/home/hamonet/Bureau/INSA/projetReseau/projet_reseau")
+
+current_dir=os.getcwd()
+os.chdir(current_dir) #a changer en fonction de l'installation    
+
+def serialize_dico( dico_to_serialize, filepath):
+    try:
+        with open(filepath, 'w') as output_file:
+            output_file.write( json.dumps( dico_to_serialize, indent = 2 ) )
+    except IOError:
+        print "serialize_dico > Impossible d'ouvrir le fichier"
+
+def deserialize_dico( filepath ):
+    try:
+        with open(filepath, 'r') as input_file:
+            return json.loads( input_file.read() )
+    except IOError:
+        print "deserialize_dico > Impossible d'ouvrir le fichier"
+
+users=deserialize_dico('comptes.json')
+permissions=deserialize_dico('permissions.json')
+
+def ajout_permissions (chaine, title) :
+    if chaine[0]=="y":
+        permissions["doctor"]["fichiers"].append(title)
+    if chaine[1]=="y":
+        permissions["nurse"]["fichiers"].append(title)
+    if chaine[2]=="y":
+        permissions["secretary"]["fichiers"].append(title)
+    serialize_dico(permissions, 'permissions.json')
+
+def check_droits(classe, nom_fichier):
+    if nom_fichier in permissions[classe]["fichiers"]:
+        return "true"
+    else:
+        return "false"
+
+def loginauth(username, password):
+    if username in users:
+        if password == users[username]["password"]:
+            print("Login successful")
+            return True
+    return False
+    
+def verify(username):
+    if users.has_key(username)==True:
+        return "false"
+    else:
+        return "true"
+    
+def create(username, password):  
+    users[username] = {}
+    users[username]["password"] = password
+    users[username]["group"] = group
+    users[username]["mail"] = []    #a completer
+    time.sleep(1)
+    print("Account has been created")
+    serialize_dico(users,'comptes.json')
+
+#arefaire
+def username_valid(recipient):
+    if users.has_key(recipient):
+        return "true"
+    else:
+        print("There is no account with that username")
+        return "false"
+
+
+
+
+def sendmail_server(username, recipient,subject,context):
+    print("Sending mail...")
+    users[recipient]["mail"].append(["Sender: " + username, "Subject: " + subject, "Context: " + context])
+    serialize_dico(users,'comptes.json')
+    time.sleep(1)
+    print("Mail has been sent to " + recipient)
+
+def receiving(file):
+    try:
+        f=open(file,"w")
+    except IOError :
+        sub.call("touch " + file, shell=True )
+    finally:
+        l = conn.recv(1024)
+        while (l.split(" ")[0]!="fin"):
+            print "receiving"
+            f.write(l)
+            l=conn.recv(BUFFER_SIZE)
+            if (l=="fin"):
+                break
+        f.close()
+
+class ClientThread(Thread):
+
+    def __init__(self,ip,port):
+        Thread.__init__(self)
+        self.ip=ip
+        self.port=port
+        print("[+] New thread started for "+ip+":"+str(port))
+
+
+    def run(self):
+        while True:
+            while True :
+                username=conn.recv(1024)
+                print username
+                if verify(username):
+                    username_exists="true"
+                    conn.send(username_exists)
+                    break
+                else:
+                    username_exists="false"
+                    conn.send(username_exists)
+            data=conn.recv(1024)
+            print("data : "+data)
+            #if not data: break
+            commande = data.split(" ")
+            print(commande)
+            if (commande[0]=="login"):
+                username=commande[1]
+                password=commande[2]
+                if loginauth(username, password):
+                    data="true"
+                    print("login auth true")
+                else:
+                    data="false"
+                    print("login auth false")
+            if (commande[0]=="register"):
+                username=commande[1]
+                password=commande[2]
+                if verify(username):
+                    create(username, password)
+                    data="created"
+                else:
+                    data="uncreated"
+            if commande[0] == "session":
+                username=commande[1]
+                if users[username]["group"] == "admin":
+                    data="role: admin"
+                    conn.send(data)
+                else:
+                    data="role: user as " + users[username]["group"]
+                    conn.send(data)
+                while True:
+                    option=conn.recv(BUFFER_SIZE)
+                    if option == "logout":
+                        print(username + " logout")
+                        break
+                    if option == "view mail":
+                        print(str(len(users[username]["mail"])))
+                        conn.send(str(len(users[username]["mail"])))
+                        time.sleep(1)
+                        for mail in users[username]["mail"]:
+                            mail=str(mail)
+                            conn.send(mail)
+                    if option == "send mail":
+                        recipient=conn.recv(BUFFER_SIZE)
+                        validity=username_valid(recipient)
+                        conn.send(validity)
+                        time.sleep(1)
+                        while validity=="false":
+                            recipient=conn.recv(BUFFER_SIZE)
+                            validity=username_valid(recipient)
+                            conn.send(validity)
+                            time.sleep(1)
+                        subject = conn.recv(BUFFER_SIZE)
+                        time.sleep(1)
+                        context = conn.recv(BUFFER_SIZE)
+                        sendmail_server(username,recipient,subject,context)
+                    if option == "commande shell":
+                        commande_shell=conn.recv(BUFFER_SIZE) #string
+                        tab_commande=commande_shell.split(' ')
+                        print(tab_commande)
+                        if tab_commande[0]=='cd':
+                            if len (tab_commande) == 1:
+                                home=expanduser("~")
+                                os.chdir(home)
+                                data=shell('pwd')
+                                conn.send(data)
+                            else:
+                                new_path=tab_commande[1]
+                                saved_path = os.getcwd()
+                                try:
+                                    os.chdir(new_path)
+                                except OSError:
+                                    print("Error : Command cancelled.")
+                                    print("Error : Command cancelled.")
+                                data=shell('pwd')
+                                conn.send(data)
+                        else:
+                            data=shell(commande_shell)
+                            conn.send(data)
+                    if option == "gestion de fichier":
+                        option_fichier = conn.recv(BUFFER_SIZE)
+                        if option_fichier=="creer un rapport":
+                            recu = ""
+                            recu = conn.recv(1024)
+                            nomFich = recu.split("NAME ")[1]
+                            nomFich = nomFich.split("OCTETS ")[0]
+                            file_access=conn.recv(BUFFER_SIZE)
+                            ajout_permissions(file_access,nomFich)
+                            taille = recu.split("OCTETS ")[1]
+                            print " >> Fichier '" + nomFich + "' [" + taille + " Ko]"
+                            taille=int(taille)
+                            num = 0
+                            while num<taille:
+                                time.sleep(1)
+                                recu = ""
+                                recu = conn.recv(1024)
+                                f = open(nomFich, "wb") 
+                                f.write(recu)                 
+                                num = num + 1024
+                            f.flush()
+                            os.fsync(f.fileno())
+                            f.close()
+                        
+
+                        elif option_fichier=="lire un rapport":
+                            nom_fichier=conn.recv(BUFFER_SIZE) #recoit le nom du rapport à lire
+                            username=conn.recv(BUFFER_SIZE) #recoit le username de l'utilisateur
+                            classe=users[username]["group"] #le serveur cherche la classe de "username" dans le dictionnaire
+                            droit=check_droits(classe, nom_fichier) #vérifie si l'utilisateur est autorisé ...
+                            conn.send(droit) # ... et envoie sa réponse au client
+                            time.sleep(1)
+                            if droit=="true":
+                                octets = os.path.getsize(nom_fichier)
+                                print(octets)
+                                conn.send("NAME " + nom_fichier + "OCTETS " + str(octets))
+                                time.sleep(1)
+                                num = 0
+                                octets = octets 
+                                fich = open(nom_fichier, "r")
+                                time.sleep(1)
+                                if octets > 1024:
+                                    for i in range(octets ):        
+                                            fich.seek(num, 0) 
+                                            donnees = fich.read(1024)    
+                                            conn.send(donnees) 
+                                            num += 1024
+                                
+                                else: 
+                                    donnees = fich.read()
+                                    conn.send(donnees)
+
+                            fich.close() 
+
+
+                    if users[username]["group"] == "admin":
+                        if option == "user mail":
+                            userinfo=conn.recv(BUFFER_SIZE)
+                            if userinfo in users:
+                                info="true"
+                                conn.send(info)
+                                time.sleep(1)
+                                conn.send(str(len(users[userinfo]["mail"])))
+                                time.sleep(1)
+                                for mail in users[userinfo]["mail"]:
+                                    mail=str(mail)
+                                    conn.send(mail)
+                            else:
+                                info="false"
+                                conn.send(info)
+
+                        elif option == "delete mail":
+                            userinfo=conn.recv(BUFFER_SIZE)
+                            if userinfo in users:
+                                info="true"
+                                conn.send(info)
+                                users[userinfo]["mail"] = []
+                                serialize_dico(users,"comptes.json")
+                                time.sleep(1)
+                                print(userinfo + "'s mail has been deleted")
+                            else:
+                                info="false"
+                                conn.send(info)
+                        elif option == "delete account":
+                            userinfo=conn.recv(BUFFER_SIZE)
+                            if userinfo in users:
+                                info="true"
+                                conn.send(info)
+                                while True:
+                                    confirm=conn.recv(BUFFER_SIZE)
+                                    if confirm == "yes":
+                                        print("Deleting " + userinfo + "'s account...")
+                                        del users[userinfo]
+                                        serialize_dico(users,"comptes.json")
+                                        time.sleep(1)
+                                        conn.send(userinfo + "'s account has been deleted")
+                                        break
+                                    elif confirm == "no":
+                                        print("Canceling account deletion...")
+                                        time.sleep(1)
+                                        conn.send("Account deletion canceled")
+                                        break
+                                    else:
+                                        conn.send(confirm + " is not an option")
+                                        break
+                            else:
+                                info="false"
+                                conn.send(info)
+
+            print("received data:", data)
+            conn.sendall(data)
+
+def shell(data):                            #Attention commande cd
+    a=sub.check_output(data, shell=True)
+    if (a==''):
+        a='commande reussie'
+    return a
+    #print 'resultat', result
+
+TCP_IP = '0.0.0.0'
+TCP_PORT = 6266
+BUFFER_SIZE = 1024
+
+##prevoir un systeme de fermeture de connexion si fermeture du client (on supprime le thread)
+##prevoir un systeme de fermeture de connexion si erreur ernno 32 broken pipe
+
+
+tcpsock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+tcpsock.bind((TCP_IP,TCP_PORT))
+threads=[]
+
+while True:
+    tcpsock.listen(5)
+    print("Waiting for incoming connections...")
+    (conn,(ip,port)) = tcpsock.accept()
+    newthread = ClientThread(ip,port)
+    newthread.start()
+    threads.append(newthread)
+
+for t in threads:
+    t.join()
